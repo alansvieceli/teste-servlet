2c67ae0f9d724942d9eabaf6659668a6a140bb79
Alan Vieceli <alansvieceli@gmail.com>
2019-02-28 23:40:23 -0300

    Ajustes para fazer deploy no Jetty

--- src/br/com/alan/gerenciador/filtros/AutorizacaoFilter.java
+++ src/br/com/alan/gerenciador/filtros/AutorizacaoFilter.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
@@ -18,9 +19,17 @@ import br.com.alan.gerenciador.acao.Acao;
 //@WebFilter("/controlador")
 public class AutorizacaoFilter implements Filter {
 
+	@Override
+	public void init(FilterConfig filterConfig) throws ServletException {
+	}
+
+	@Override
+	public void destroy() {
+	}
+
 	public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain chain)
 			throws IOException, ServletException {
-		
+
 		System.out.println("Autorização...");
 
 		HttpServletRequest request = (HttpServletRequest) servletRequest;
--- src/br/com/alan/gerenciador/filtros/MonitoramentoFilter.java
+++ src/br/com/alan/gerenciador/filtros/MonitoramentoFilter.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
@@ -15,9 +16,17 @@ import javax.servlet.annotation.WebFilter;
 //@WebFilter("/controlador") nao é possivel definir a ordem dos filter, pra isso usar web.xmlagora sim
 public class MonitoramentoFilter implements Filter {
 
+	@Override
+	public void init(FilterConfig filterConfig) throws ServletException {
+	}
+
+	@Override
+	public void destroy() {
+	}
+
 	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
 			throws IOException, ServletException {
-		
+
 		System.out.println("Monitoramento...");
 
 		String controle = request.getParameter("acao");
